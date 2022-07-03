617ae778959e5b074672e74accf7849043a94991
柱子 <wenmin92@gmail.com>
2022-07-03 20:10:51 +0800

    apply official template

--- Scoop-Bucket.Tests.ps1
+++ Scoop-Bucket.Tests.ps1
@@ -1,2 +1,2 @@
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
-Invoke-Pester "$psscriptroot/.."
+. "$env:SCOOP_HOME\test\Import-Bucket-Tests.ps1"
--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,9 +1,9 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "mo-san/scoop-bucket:main"
+    [String]$upstream = "<username>/<bucketname>:main"
 )
 
-if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
-$autopr = "$psscriptroot/auto-pr_mod.ps1"
-$dir = "$psscriptroot/../bucket" # checks the parent dir
-Invoke-Expression -command "& '$autopr' -dir '$dir' -upstream $upstream $($args | ForEach-Object { "$_ " })"
+if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
+$autopr = "$env:SCOOP_HOME/bin/auto-pr.ps1"
+$dir = "$PSScriptRoot/../bucket" # checks the parent dir
+Invoke-Expression -Command "& '$autopr' -dir '$dir' -upstream $upstream $($args | ForEach-Object { "$_ " })"
